0f78fbf1488b93be4ec5e5dbee59874f47895f4a
SerjAyvazyan-2000 <ayvazyanserj3@gmail.com>
2025-01-23 23:29:55 +0400

    new

---
+++ assets/js/swiper.js
@@ -0,0 +1,51 @@
+
+
+
+
+let reviewsSwiper = new Swiper(".reviews-swiper", {
+    spaceBetween: 24,
+    slidesPerView: 3,
+    loop:true,
+
+    navigation: {
+        nextEl: '.reviews-button-prev',
+        prevEl: '.reviews-button-next',
+    },
+    breakpoints: {
+        320: {slidesPerView: 1,},
+        540: {slidesPerView: 1,},
+        576: {slidesPerView: 1.5},
+        779: {slidesPerView: 2},
+        954: {slidesPerView: 2.4},
+        1139: {slidesPerView: 2.5},
+        1200: {slidesPerView: 3},
+
+    },
+
+
+
+});
+let advantagesSwiper = new Swiper(".advantages-swiper", {
+    spaceBetween: 24,
+    slidesPerView: 3,
+    loop:true,
+
+    navigation: {
+        nextEl: '.reviews-button-prev',
+        prevEl: '.reviews-button-next',
+    },
+    breakpoints: {
+        320: {slidesPerView: 1,},
+        540: {slidesPerView: 1,},
+        576: {slidesPerView: 1.5},
+        779: {slidesPerView: 2},
+        954: {slidesPerView: 2.4},
+        1139: {slidesPerView: 2.5},
+        1200: {slidesPerView: 3},
+
+    },
+
+
+
+});
+
